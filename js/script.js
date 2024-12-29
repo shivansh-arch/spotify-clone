40a69352a7fb30191d3d1d25685b8256d3ce8609
shivansh-arch <shivansh.gupta2024@lpu.in>
2024-12-30 00:03:20 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ console.log('Lets write JavaScript');
 let currentSong = new Audio();
 let songs;
 let currFolder;
-let playButton = document.getElementById("play");
+
 if (chrome.runtime && chrome.runtime.sendMessage) { chrome.runtime.sendMessage({greeting: "hello"}, function(response) { console.log(response); }); } else { console.error("Receiving end does not exist."); }
 
 
